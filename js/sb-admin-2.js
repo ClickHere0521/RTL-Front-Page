d55c9934c873eb90fa041ccbcd0666da3aaf1626
ClickHere0521 <clickhere0901@outlook.com>
2021-05-02 07:31:14 -0500

    login 50%

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -1,34 +1,54 @@
-$(function() {
-  $('#side-menu').metisMenu();
+$(function () {
+  $("#side-menu").metisMenu();
 });
 
 // Loads the correct sidebar on window load,
 // collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
-$(function() {
-  $(window).bind("load resize", function() {
+$(function () {
+  $(window).bind("load resize", function () {
     topOffset = 50;
-    width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+    width =
+      this.window.innerWidth > 0 ? this.window.innerWidth : this.screen.width;
     if (width < 768) {
-      $('div.navbar-collapse').addClass('collapse');
+      $("div.navbar-collapse").addClass("collapse");
       topOffset = 100; // 2-row-menu
     } else {
-      $('div.navbar-collapse').removeClass('collapse');
+      $("div.navbar-collapse").removeClass("collapse");
     }
 
-    height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+    height =
+      (this.window.innerHeight > 0
+        ? this.window.innerHeight
+        : this.screen.height) - 1;
     height = height - topOffset;
     if (height < 1) height = 1;
     if (height > topOffset) {
-      $("#page-wrapper").css("min-height", (height) + "px");
+      $("#page-wrapper").css("min-height", height + "px");
     }
   });
 
   var url = window.location;
-  var element = $('ul.nav a').filter(function() {
-    return this.href == url || url.href.indexOf(this.href) == 0;
-  }).addClass('active').parent().parent().addClass('in').parent();
-  if (element.is('li')) {
-    element.addClass('active');
+  var element = $("ul.nav a")
+    .filter(function () {
+      return this.href == url || url.href.indexOf(this.href) == 0;
+    })
+    .addClass("active")
+    .parent()
+    .parent()
+    .addClass("in")
+    .parent();
+  if (element.is("li")) {
+    element.addClass("active");
   }
+});
+
+$("#btn1").click(function () {
+  $("#btn1").css({ "background-color": "#485272", color: "white" });
+  $("#btn2").css({ "background-color": "#bfc5d2", color: "#78849e" });
+});
+
+$("#btn2").click(function () {
+  $("#btn2").css({ "background-color": "#485272", color: "white" });
+  $("#btn1").css({ "background-color": "#bfc5d2", color: "#78849e" });
 });
