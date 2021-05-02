55f863c4059dabd514b9090f1bc126b2664a3ed9
ClickHere0521 <clickhere0901@outlook.com>
2021-05-02 12:56:42 -0500

    login complete

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -42,13 +42,62 @@ $(function () {
     element.addClass("active");
   }
 });
+var flag = true;
 
 $("#btn1").click(function () {
   $("#btn1").css({ "background-color": "#485272", color: "white" });
   $("#btn2").css({ "background-color": "#bfc5d2", color: "#78849e" });
+  $("#content1").css("display", "block");
+  $("#content2").css("display", "none");
+  $("#content3").css("display", "none");
+  flag=true;
 });
 
 $("#btn2").click(function () {
   $("#btn2").css({ "background-color": "#485272", color: "white" });
   $("#btn1").css({ "background-color": "#bfc5d2", color: "#78849e" });
+  $("#checkbox-2").css("display", "none");
+  $("#checkbox-1").css("display", "block");
+  $("#content1").css("display", "none");
+  $("#content2").css("display", "block");
+  $("#content3").css("display", "none");
+  flag=false;
+});
+
+$("#forgot-name").click(function () {
+  $(".cus-input-1").attr("placeholder", "תעודת זהות");
+  $(".cus-input-2").attr("placeholder", "תאריך לידה");
+  $(".cus-input-3").attr("placeholder", "מספר חשבון");
+  $(".btn-confirm-2").css("display", "block");
+  $(".checkbox").css("display", "none");
+  $(".cus-panel-header-1").css("display", "block");
+  $(".cus-panel-header-2").css("display", "none");
+  $(".forgot-name").css("display", "none");
+  $(".p-2").css("display", "none");
+  $(".cus-form-1").css("margin-bottom", "31px");
+  $(".cus-form-3").css("margin-bottom", "31px");
+});
+
+$(".btn-confirm-2").click(function () {
+  $(".cus-input-1").attr("placeholder", "שם משתמש");
+  $(".cus-input-2").attr("placeholder", "תעודת זהות");
+  $(".cus-input-3").attr("placeholder", "סיסמה");
+  $(".btn-confirm-2").css("display", "none");
+  $(".checkbox").css("display", "block");
+  $(".cus-panel-header-1").css("display", "none");
+  $(".cus-panel-header-2").css("display", "block");
+  $(".forgot-name").css("display", "block");
+  $(".p-2").css("display", "block");
+  $(".cus-form-1").css("margin-bottom", "0px");
+  $(".cus-form-3").css("margin-bottom", "0px");
+});
+
+$(".btn-confirm").click(function () {
+  if(flag) {
+    $("#checkbox-2").css("display", "block");
+    $("#checkbox-1").css("display", "none");
+  }
+  $("#content3").css("display", "block");
+  $("#content1").css("display", "none");
+  $("#content2").css("display", "none");
 });
